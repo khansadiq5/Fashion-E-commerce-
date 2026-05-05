66fd3e2e36c643e7ff481ea1234c0777fd4dacec
Khan Sadiq <k.sadiq5426@gmail.com>
2026-05-05 20:30:35 +0530

    Add categories migration

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-    //
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'image',
+        'status',
+    ];
 }
